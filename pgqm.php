d91f02dcaffe0c27ef7942a57ab9b5984d7f1858
Everton Berz <everton.berz@gmail.com>
2019-11-26 19:02:51 -0300

    Fixed mail alerts to work with containers

--- pgqm.php
+++ pgqm.php
@@ -11,6 +11,13 @@ ini_set('display_errors', 1);
 set_time_limit(0);
 date_default_timezone_set("America/Sao_Paulo");
 
+include('PHPMailer/PHPMailer.php');
+include('PHPMailer/SMTP.php');
+include('PHPMailer/Exception.php');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 define("VERSION", "1.0");
 print("Starting PGQM (PostgreSQL Query Monitor) - Version ".VERSION.PHP_EOL);
 
@@ -27,9 +34,12 @@ define("ITERATION_RANGE_WHILE_CRISIS", $ini["general"]["iterationRangeWhileCrisi
 define("CRISIS_DURATION_FOR_ALERT", $ini["general"]["crisisDurationForAlert"]);
 define("WAIT_TIME_FOR_ACTION", $ini["general"]["waitTimeForAction"]);
 define("ALERT_MAIL_RECIPIENT", $ini["general"]["alertMailRecipient"]);
+define("ALERT_MAIL_SENDER", $ini["general"]["alertMailSender"]);
 define("SQLITE_DB_PATH", $ini["general"]["sqliteDbPath"]);
 define("SQLITE_DB_MAX_SIZE", $ini["general"]["sqliteDbMaxSize"]);
 define("TAR_PATH", $ini["general"]["tarPath"]);
+define("SMTP_HOST", $ini["general"]["smtpHost"]);
+define("SMTP_PORT", $ini["general"]["smtpPort"]);
 
 $triggerServerList = @explode(",", $ini["trigger"]["servers"]);
 define("TRIGGER_COMMAND", $ini["trigger"]["command"]);
@@ -51,7 +61,7 @@ while (true) {  // postgresql connect loop (for reconnections)
   if (!$pgConnection) {
     print("Error while connectiong to PostgreSQL database: host=$infoPostgresqlDb[host] port=$infoPostgresqlDb[port] dbname=$infoPostgresqlDb[dbname] user=$infoPostgresqlDb[user]".PHP_EOL);
     sleep(3);
-    continue; 
+    continue;
   }
 
   $pgVersion = get_postgresql_server_version($pgConnection);
@@ -65,21 +75,21 @@ while (true) {  // postgresql connect loop (for reconnections)
   }
 
   $stallFilter = "and (state = 'active' or $sqlWhereWaitEvent) and age(now(), query_start) > cast($1 as interval)";
-  $sqlMonitor = "select 
-      datname, 
-      pid, 
-      client_addr, 
-      usename, 
-      extract(epoch from query_start) as query_start, 
-      trunc(extract(epoch from age(now(), query_start))) as timediff, 
+  $sqlMonitor = "select
+      datname,
+      pid,
+      client_addr,
+      usename,
+      extract(epoch from query_start) as query_start,
+      trunc(extract(epoch from age(now(), query_start))) as timediff,
       $sqlWaitEvent,
       state,
       query,
-      application_name     
-    from pg_stat_activity 
-    where   
-     pid <> pg_backend_pid() 
-     ###stallFilter### 
+      application_name
+    from pg_stat_activity
+    where
+     pid <> pg_backend_pid()
+     ###stallFilter###
      order by query_start";
   $sqlMonitorFiltered = str_replace("###stallFilter###", $stallFilter, $sqlMonitor);
   $sqlMonitorNoFilter = str_replace("###stallFilter###", "", $sqlMonitor);
@@ -95,7 +105,7 @@ while (true) {  // postgresql connect loop (for reconnections)
 
   $iteration = 0;
   $iterationRange = DEFAULT_ITERATION_RANGE;
-  while (true) { 
+  while (true) {
 
     $stat = pg_connection_status($pgConnection);
     if ($stat !== 0) {
@@ -112,7 +122,7 @@ while (true) {  // postgresql connect loop (for reconnections)
     $batchId = time();
     if ($recordCount >= QUERY_COUNT_ABOVE_THRESHOLD) {
       $iterationRange=ITERATION_RANGE_WHILE_CRISIS;
-      print(PHP_EOL."[".date("d-m-Y H:i:s", $batchId)."] $recordCount queries lentas encontradas ($batchId). Guardando snapshot (".pg_num_rows($resMonitorNoFilter)." registros)... ");    
+      print(PHP_EOL."[".date("d-m-Y H:i:s", $batchId)."] $recordCount queries lentas encontradas ($batchId). Guardando snapshot (".pg_num_rows($resMonitorNoFilter)." registros)... ");
 
       while ($line = pg_fetch_array($resMonitorNoFilter)) {
         $pInsertSqlite->bindValue(':mtimestamp', $batchId, SQLITE3_INTEGER);
@@ -134,7 +144,7 @@ while (true) {  // postgresql connect loop (for reconnections)
       }
 
       $crisisDuration= ($batchId-$batchIdFromFirstDetectionOfTheDefect);
-      if ($recordCount >= QUERY_COUNT_ABOVE_THRESHOLD_ALERT and 
+      if ($recordCount >= QUERY_COUNT_ABOVE_THRESHOLD_ALERT and
           $crisisDuration > CRISIS_DURATION_FOR_ALERT) {
         print(PHP_EOL);
         $message = "PGQM (PostgreSQL Query Monitor)".PHP_EOL.
@@ -159,52 +169,51 @@ while (true) {  // postgresql connect loop (for reconnections)
         "PID | Application name | Duração | Query".PHP_EOL;
 
         $sql = "select query, timediff, pid, application_name
-                from pgqm 
-                where mtimestamp = $batchId 
+                from pgqm
+                where mtimestamp = $batchId
                 and timediff > ".QUERY_DURATION_THRESHOLD." and
-                  (state = 'active' or wait_event <> 'ClientRead') 
+                  (state = 'active' or wait_event <> 'ClientRead')
                 order by timediff desc";
         $pSelMsg = $sqliteConnection->query($sql);
-        
+
         while ($detailLine = $pSelMsg->fetchArray()) {
           $message .= "$detailLine[pid] | $detailLine[application_name] | $detailLine[timediff] | ".substr($detailLine["query"], 0, 120).PHP_EOL;
         }
         $message .= PHP_EOL."FIM!";
         print($message.PHP_EOL.PHP_EOL.PHP_EOL);
 
-        if (time()-$lastSentMessageTimestamp >= WAIT_TIME_FOR_ACTION or 
+        if (time()-$lastSentMessageTimestamp >= WAIT_TIME_FOR_ACTION or
            $lastSentMessageTimestamp == null) {
 
           // Only send emails between 6 am and 23 am
           if (date('G') > 5) {
-            mail(ALERT_MAIL_RECIPIENT, "PGQM Alerta", $message);
+            send_email(SMTP_HOST, SMTP_PORT, ALERT_MAIL_SENDER, ALERT_MAIL_RECIPIENT, "PGQM Alerta", $message);
             $lastSentMessageTimestamp=time();
-          
+
             // trigger
-            foreach ($triggerServerList as $remoteServer) {
-              print("Triggering $remoteServer...".PHP_EOL);
-              
-              $triggerCommand = TRIGGER_COMMAND;
-              $triggerCommand = str_replace("###servidor###", $remoteServer, $triggerCommand);
-              $triggerCommand = str_replace("###identificador###", $batchId, $triggerCommand);
-              print("Running: $triggerCommand".PHP_EOL);
-              $lastLine = exec($triggerCommand, $output, $returnCode);
-              print("  Output: ".implode("#", $output).PHP_EOL);
-              print("  Last line: $lastLine".PHP_EOL);
-              print("  Return code: $returnCode".PHP_EOL);
-              print(PHP_EOL);
+            if (strlen(TRIGGER_COMMAND) > 0) {
+              foreach ($triggerServerList as $remoteServer) {
+                print("Triggering $remoteServer...".PHP_EOL);
 
+                $triggerCommand = TRIGGER_COMMAND;
+                $triggerCommand = str_replace("###servidor###", $remoteServer, $triggerCommand);
+                $triggerCommand = str_replace("###identificador###", $batchId, $triggerCommand);
+                print("Running: $triggerCommand".PHP_EOL);
+                $lastLine = exec($triggerCommand, $output, $returnCode);
+                print("  Output: ".implode("#", $output).PHP_EOL);
+                print("  Last line: $lastLine".PHP_EOL);
+                print("  Return code: $returnCode".PHP_EOL);
+                print(PHP_EOL);
+
+              }
+              print("Trigger commands sent to remote servers!".PHP_EOL.PHP_EOL);
             }
-            print("Trigger commands sent to remote servers!".PHP_EOL.PHP_EOL);
-            // fim acao
-
           }
-
         }
 
       }
 
-    } else {    
+    } else {
       print(".");
       $iterationRange=DEFAULT_ITERATION_RANGE;
       $batchIdFromFirstDetectionOfTheDefect=$batchId;
@@ -221,7 +230,7 @@ while (true) {  // postgresql connect loop (for reconnections)
 
 
   } // query loop
-  
+
 } // postgresql connection loop
 
 function connect_sqlite_database() {
@@ -230,12 +239,12 @@ function connect_sqlite_database() {
   //print("Connecting to SQLite database...".PHP_EOL);
   $sqliteConnection = new SQLite3(SQLITE_DB_PATH);
   //$sqliteConnection->exec("DROP TABLE pgqm");
-  if ($sqliteConnection->querySingle("SELECT name FROM sqlite_master WHERE type='table' AND name='pgqm'") == null) {  
+  if ($sqliteConnection->querySingle("SELECT name FROM sqlite_master WHERE type='table' AND name='pgqm'") == null) {
     print("  New database detected. Creating table...".PHP_EOL);
-    $sqliteConnection->exec("CREATE TABLE pgqm (mtimestamp integer, datname text, pid integer, usename text, client_addr text, query_start integer, 
+    $sqliteConnection->exec("CREATE TABLE pgqm (mtimestamp integer, datname text, pid integer, usename text, client_addr text, query_start integer,
       timediff integer, wait_event text, state text, query text, threshold integer, application_name text)");
   }
-  $pInsertSqlite = $sqliteConnection->prepare("INSERT INTO pgqm (mtimestamp, datname, pid, usename, client_addr, query_start, timediff, wait_event, state, query, threshold, application_name) 
+  $pInsertSqlite = $sqliteConnection->prepare("INSERT INTO pgqm (mtimestamp, datname, pid, usename, client_addr, query_start, timediff, wait_event, state, query, threshold, application_name)
     VALUES (:mtimestamp, :datname, :pid, :usename, :client_addr, :query_start, :timediff, :wait_event, :state, :query, :threshold, :application_name)");
 }
 
@@ -247,9 +256,9 @@ function check_sqlite_database_size() {
   }
 
   clearstatcache();
-  $dbsize = filesize(SQLITE_DB_PATH);  
+  $dbsize = filesize(SQLITE_DB_PATH);
   if ($dbsize > SQLITE_DB_MAX_SIZE) {
-    print(PHP_EOL."*** SQLite database size excedeed ($dbsize > ".SQLITE_DB_MAX_SIZE.". Rotating...".PHP_EOL);       
+    print(PHP_EOL."*** SQLite database size excedeed ($dbsize > ".SQLITE_DB_MAX_SIZE.". Rotating...".PHP_EOL);
     $outputFile = SQLITE_DB_PATH."-".date("Ymd-His").".tar.bz2";
     $cmd = escapeshellcmd(TAR_PATH)." -jcvf $outputFile ".escapeshellarg(SQLITE_DB_PATH);
     print("Running: $cmd".PHP_EOL);
@@ -260,7 +269,7 @@ function check_sqlite_database_size() {
     print("  Return code: $returnCode".PHP_EOL);
     print(PHP_EOL);
     if ($returnCode == 0) {
-      unlink(SQLITE_DB_PATH);      
+      unlink(SQLITE_DB_PATH);
     }
   }
 
@@ -271,9 +280,38 @@ function get_postgresql_server_version($pgConnection) {
   $sql = "select version()";
   $resVersion = pg_query($pgConnection, $sql);
   $versionDetails = pg_fetch_result($resVersion, 0, "version");
-  $version = substr($versionDetails, strpos($versionDetails, " ") + 1); 
-  $version = substr($version, 0, strpos($version, " ")); 
+  $version = substr($versionDetails, strpos($versionDetails, " ") + 1);
+  $version = substr($version, 0, strpos($version, " "));
   return $version;
+}
+
+function send_email($smtpHost, $smtpPort, $sender, $recipient, $subject, $message) {
+  $mail = new PHPMailer(true);
+  try {
+      //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+      $mail->isSMTP();
+      $mail->Host       = $smtpHost;
+      $mail->SMTPAuth   = false;
+      //$mail->Username   = 'user@example.com';
+      //$mail->Password   = 'secret';
+      //$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+      $mail->SMTPSecure = false;
+      $mail->SMTPAutoTLS = false;
+      $mail->Port       = $smtpPort;
+
+      $mail->setFrom($sender, 'PGQM Alerta');
+      $mail->addAddress($recipient);
+
+      $mail->isHTML(false);
+      $mail->Subject = $subject;
+      $mail->Body    = $message;
+
+      $mail->send();
+      echo 'Message has been sent';
+  } catch (Exception $e) {
+      echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+  }
+
 }
 
 ?>
